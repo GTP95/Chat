3df5fe368e9d3a7b2d643c627fa3a4801d60adab
Giacomo Tommaso Petrucci <giacomo.tom.petrucci@tiscali.it>
2018-04-17 16:25:59 +0200

    Refactor utile

---
+++ src/com/server/TestClassPull.java
@@ -0,0 +1,11 @@
+package com.server;
+
+public class TestClassPull {
+    public void setCoseUtili(String coseUtili) {
+        this.coseUtili = coseUtili;
+    }
+
+    private String coseUtili;
+
+
+}
